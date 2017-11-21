16c608bd158e20c28d4e3e83a4d28d9e933dfcfa
Dietrich Epp Schmidt <djeppschmidt@gmail.com>
2017-11-21 13:21:31 -0500

    Create SequenceVariants.R
    Template for generating an SOP for inferring sequence variants

---
+++ SequenceVariants.R
@@ -0,0 +1,34 @@
+# copy and pasted directly from tutorial: https://benjjneb.github.io/dada2/bigdata_paired.html
+
+library(dada2); packageVersion("dada2")
+# File parsing
+filtpathF <- "/path/to/FWD/filtered" # CHANGE ME to the directory containing your filtered forward fastq files
+filtpathR <- "/path/to/FWD/filtered" # CHANGE ME ...
+filtFs <- list.files(filtpathF, pattern="fastq.gz", full.names = TRUE)
+filtRs <- list.files(filtpathR, pattern="fastq.gz", full.names = TRUE)
+sample.names <- sapply(strsplit(basename(filtFs), "_"), `[`, 1) # Assumes filename = samplename_XXX.fastq.gz
+sample.namesR <- sapply(strsplit(basename(filtRs), "_"), `[`, 1) # Assumes filename = samplename_XXX.fastq.gz
+if(!identical(sample.names, sample.namesR)) stop("Forward and reverse files do not match.")
+names(filtFs) <- sample.names
+names(filtRs) <- sample.names
+set.seed(100)
+# Learn forward error rates
+errF <- learnErrors(filtFs, nread=1e6, multithread=TRUE)
+# Learn reverse error rates
+errR <- learnErrors(filtRs, nread=1e6, multithread=TRUE)
+# Sample inference and merger of paired-end reads
+mergers <- vector("list", length(sample.names))
+names(mergers) <- sample.names
+for(sam in sample.names) {
+  cat("Processing:", sam, "\n")
+    derepF <- derepFastq(filtFs[[sam]])
+    ddF <- dada(derepF, err=errF, multithread=TRUE)
+    derepR <- derepFastq(filtRs[[sam]])
+    ddR <- dada(derepR, err=errR, multithread=TRUE)
+    merger <- mergePairs(ddF, derepF, ddR, derepR)
+    mergers[[sam]] <- merger
+}
+rm(derepF); rm(derepR)
+# Construct sequence table and remove chimeras
+seqtab <- makeSequenceTable(mergers)
+saveRDS(seqtab, "/path/to/run1/output/seqtab.rds") # CHANGE ME to where you want sequence table saved
